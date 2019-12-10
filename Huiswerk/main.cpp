92e310d6d9689f8343530f6ecfb64c5ffb5d9e81
erikjandz <43314178+erikjandz@users.noreply.github.com>
2019-12-10 09:38:39 +0100

    update

--- Huiswerk/main.cpp
+++ Huiswerk/main.cpp
@@ -1,32 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <algorithm>
+#include <string>
 #include <cctype>
 #include <map>
+#include <sstream> 
 
-int main(){
+int main() {
 	//1
 	std::ifstream input("the-king-james-bible.txt");
-	std::string s = " ";
-	std::vector<char> bible = {};
-	while (input) {
-		input >> s;
-		for (auto c : s) {
-			bible.push_back(c);
-		}
-	}
+	input >> std::noskipws;
+	std::vector<char> bible{
+		std::istream_iterator<char>{input},
+		std::istream_iterator<char>{}
+	};
+
 	//2
 	std::cout << "size = " << bible.size() << std::endl;
-	
+
 	//3
-	std::cout << "amount of newlines: " <<std::count(bible.begin(), bible.end(), '\n') << std::endl;
+	std::cout << "amount of newlines: " << std::count(bible.begin(), bible.end(), '\n') << std::endl;
 
 	//4
 	int count = 0;
 	std::string alfabet = "abcdefghijklmnopqrstuvwxyz";
 	std::for_each(alfabet.begin(), alfabet.end(), [&](char& c) { count += std::count(bible.begin(), bible.end(), c); });
-	std::cout << "amount of a to z " <<count << std::endl;
+	std::cout << "amount from a to z " << count << std::endl;
 
 	//5
 	std::for_each(bible.begin(), bible.end(), [&](char& c) { c = std::tolower(c); });
@@ -39,8 +40,8 @@ int main(){
 	std::for_each(alfabet.begin(), alfabet.end(), [&](char& c) { letters.push_back(std::count(bible.begin(), bible.end(), c)); });
 
 	//7.1
-	std::cout << "every amount form a to z: " << std::endl;
-	std::for_each(letters.begin(), letters.end(), [](int & c) {std::cout << c << std::endl; });
+	std::cout << "every amount from a to z: " << std::endl;
+	std::for_each(letters.begin(), letters.end(), [](int& c) {std::cout << c << std::endl; });
 	//7.2
 	std::cout << "every amount sorted form a to z: " << std::endl;
 	std::sort(letters.begin(), letters.end());
@@ -52,17 +53,29 @@ int main(){
 	std::string s2;
 	while (input2) {
 		input2 >> s2;
-		if (dic.count(s2) > 0){
+		if (dic.count(s2) > 0) {
 			dic[s2] ++;
-		}else {
+		}
+		else {
 			dic[s2] = 1;
 		}
 	}
-		/*auto x = std::max_element(dic.begin(), dic.end(),
-			[](const decltype(dic)::value_type<std::string, int>& p1, const pair<std::string, int > & p2) {
-			return p1.second < p2.second; });
-		std::cout << x->first;*/
+
+	std::string word;
+	int current_max;
+	for (unsigned int i = 0; i < 10; i++) {
+		current_max = 0;
+		for (auto i : dic) {
+			if (i.second > current_max){
+				current_max = i.second;
+				word = i.first;
+			}
+		}
+		std::cout << word << "\n";
+		dic.erase(word);
+	}
+	/*
 	for (auto i : dic) {
 		std::cout << i.first << " " << i.second << std::endl;
-	}
+	}*/
 }
